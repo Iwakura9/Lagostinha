f657ce4f665732899490ec88e7e6b0f4a9eaced6
eric-r-g <eric.rodrig.gomes@gmail.com>
2025-06-14 19:02:40 -0300

    Update views.py

--- Lagostinha/gabaritos/views.py
+++ Lagostinha/gabaritos/views.py
@@ -1,13 +1,26 @@
+import requests  # Importa a biblioteca para fazer requisições HTTP
+from django.conf import settings  # Para acessar as configurações do projeto
 from django.shortcuts import render
+
 
 # Create your views here.
 
 gabaritos = ["joão", "Fernando", "cleber"] # temporario, vai receber a tabela de gabaritos
 
 def index(request):
+    try:
+        response = requests.get(f'{settings.API_BASE_URL}/participantes/')
+        response.raise_for_status() 
+        participantes = response.json()
+
+    except requests.exceptions.RequestException as e:
+        participantes = []
+
     return render(request, "gabaritos/index.html", {
-        "gabaritos":gabaritos
-    }) # renderiza a pagina principal
+        "participantes": participantes
+    })
+    
 
 def add(request):
+    
     return render(request, "gabaritos/add.html")
