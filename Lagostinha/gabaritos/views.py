9ecc68363c0a292f8c836f69f7363df87baeefb3
eric-r-g <eric.rodrig.gomes@gmail.com>
2025-06-14 21:00:56 -0300

    Update views.py

--- Lagostinha/gabaritos/views.py
+++ Lagostinha/gabaritos/views.py
@@ -1,12 +1,10 @@
 import requests  # Importa a biblioteca para fazer requisições HTTP
 from django.conf import settings  # Para acessar as configurações do projeto
 from django.shortcuts import render
-from .forms import ParticipanteForms
-
+from .forms import AlunoForm
+from core.models import *
 
 # Create your views here.
-
-gabaritos = ["joão", "Fernando", "cleber"] # temporario, vai receber a tabela de gabaritos
 
 def index(request):
     try:
@@ -23,5 +21,61 @@ def index(request):
     
 
 def add(request):
+    mensagem = None
+    tipo_mensagem = None # erro, sucesso, ou None
 
-    return render(request, "gabaritos/add.html")
+    if request.method == "POST":
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+
+            nome_aluno = form.cleaned_data['nome_aluno'] 
+            nome_escola = form.cleaned_data['nome_escola']
+            
+            try:
+                # Verifica se a escola existe
+                response = requests.get(
+                    f'{settings.API_BASE_URL}/escolas/',
+                    params={'nome__iexact': nome_escola}
+                )
+                response.raise_for_status()  # Verifica erros na requisição
+                escolas = response.json()
+
+                if escolas:  # Escola existe
+                    escola_id = escolas[0]['id']
+                    mensagem = f"Aluno vinculado à escola existente: {nome_escola}"
+                else:  # Cria nova escola
+                    response = requests.post(
+                        f"{settings.API_BASE_URL}/escolas/",
+                        json={'nome': nome_escola}
+                    )
+                    response.raise_for_status()
+                    escola_id = response.json()['id']
+                    mensagem = f"Nova escola cadastrada: {nome_escola}"
+                
+                # Cadastra o aluno
+                response = requests.post(
+                    f"{settings.API_BASE_URL}/participantes/",
+                    json={
+                        'nome': nome_aluno,
+                        'escola': escola_id
+                    }
+                )
+                response.raise_for_status()
+                
+                tipo_mensagem = 'sucesso'
+                form = AlunoForm() 
+
+            except requests.exceptions.RequestException as e:
+                mensagem = "Erro na comunicação com a API:" + str(e)
+                tipo_mensagem = "erro"
+            except Exception as e:
+                mensagem = "Erro no cadastro: " + str(e)
+                tipo_mensagem = "erro"
+    else:
+        form = AlunoForm()
+    
+    return render(request, "gabaritos/add.html",{
+        'form': form,
+        'mensagem': mensagem,
+        'tipo_mensagem': tipo_mensagem
+    })
