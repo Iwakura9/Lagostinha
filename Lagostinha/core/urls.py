9fd8cb796d7617240f6e9d3f8eef9265db50ecfa
Sousa <trsh9sousa@gmail.com>
2025-06-14 21:39:07 -0300

    correção de bugs relacionados a url

--- Lagostinha/core/urls.py
+++ Lagostinha/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EscolaViewSet, ParticipanteViewSet, ProvaViewSet, LeituraViewSet
+from .views import EscolaViewSet, ParticipanteViewSet, ProvaViewSet, LeituraViewSet, GabaritoUploadView
 
 # URL routing para a API do Lagostinha.
 router = DefaultRouter()
@@ -10,6 +10,7 @@ router.register(r'provas', ProvaViewSet)
 router.register(r'leituras', LeituraViewSet)
 # cria automaticamente as URLs para os viewsets registrados
 
-urlpatterns = [ 
-    path('', include(router.urls)),
+urlpatterns = router.urls
+urlpatterns += [ 
+    path('upload-gabarito/', GabaritoUploadView.as_view(), name='upload-gabarito')
 ]
